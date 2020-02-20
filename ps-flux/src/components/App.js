1bab84494c311b04f89dcca87a58a5cfeccf23ab
Zach Hollis <43797367+zhollis21@users.noreply.github.com>
2020-02-20 16:45:24 -0500

    Completed Module 8

--- ps-flux/src/components/App.js
+++ ps-flux/src/components/App.js
@@ -3,19 +3,22 @@ import HomePage from "./HomePage";
 import AboutPage from "./AboutPage";
 import Header from "./common/Header";
 import CoursesPage from "./CoursesPage";
+import NotFoundPage from "./NotFoundPage";
+import ManageCoursePage from "./ManageCoursePage";
+import { Route, Switch, Redirect } from "react-router-dom";
 
 function App() {
-  function getPage() {
-    const route = window.location.pathname;
-
-    if (route === "/courses") return <CoursesPage />;
-    if (route === "/about") return <AboutPage />;
-    return <HomePage />;
-  }
   return (
     <div className="container-fluid">
       <Header />
-      {getPage()}
+      <Switch>
+        <Route path="/" exact component={HomePage} />
+        <Route path="/courses" component={CoursesPage} />
+        <Route path="/course/:slug" component={ManageCoursePage} />
+        <Route path="/about" component={AboutPage} />
+        <Redirect from="/about-page" to="about" />
+        <Route component={NotFoundPage} />
+      </Switch>
     </div>
   );
 }

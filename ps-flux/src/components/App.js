f693e8724e86248e65e7e0cc004b8279323b8f17
Zach Hollis <43797367+zhollis21@users.noreply.github.com>
2020-02-20 12:36:38 -0500

    Completed Module 6

--- ps-flux/src/components/App.js
+++ ps-flux/src/components/App.js
@@ -2,13 +2,15 @@ import React from "react";
 import HomePage from "./HomePage";
 import AboutPage from "./AboutPage";
 import Header from "./common/Header";
+import CoursesPage from "./CoursesPage";
 
 function App() {
   function getPage() {
     const route = window.location.pathname;
 
+    if (route === "/courses") return <CoursesPage />;
     if (route === "/about") return <AboutPage />;
-    else return <HomePage />;
+    return <HomePage />;
   }
   return (
     <div className="container-fluid">

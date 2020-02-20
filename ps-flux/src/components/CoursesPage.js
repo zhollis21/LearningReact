652f738f60c09ef8b3fb1fa53c0e481385f7cf3d
Zach Hollis <43797367+zhollis21@users.noreply.github.com>
2020-02-20 14:43:46 -0500

    Completed Module 7

--- ps-flux/src/components/CoursesPage.js
+++ ps-flux/src/components/CoursesPage.js
@@ -1,42 +1,20 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { getCourses } from "../api/courseApi";
+import CourseList from "./CourseList";
 
-class CoursesPage extends React.Component {
-  state = {
-    courses: []
-  };
+function CoursesPage() {
+  const [courses, setCourses] = useState([]);
 
-  componentDidMount() {
-    getCourses().then(courses => this.setState({ courses: courses }));
-  }
+  useEffect(() => {
+    getCourses().then(_courses => setCourses(_courses));
+  }, []);
 
-  render() {
-    return (
-      <>
-        <h2>Courses</h2>
-        <table className="table">
-          <thead>
-            <tr>
-              <th>Title</th>
-              <th>Author Id</th>
-              <th>Category</th>
-            </tr>
-          </thead>
-          <tbody>
-            {this.state.courses.map(course => {
-              return (
-                <tr key={course.id}>
-                  <td>{course.title}</td>
-                  <td>{course.authorId}</td>
-                  <td>{course.category}</td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
-      </>
-    );
-  }
+  return (
+    <>
+      <h2>Courses</h2>
+      <CourseList courses={courses} />
+    </>
+  );
 }
 
 export default CoursesPage;

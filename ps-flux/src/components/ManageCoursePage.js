c6dbbafe95f7e482f66e7d7a440a8c599428ccd1
Zach Hollis <43797367+zhollis21@users.noreply.github.com>
2020-02-21 16:10:41 -0500

    Completed Module 9

--- ps-flux/src/components/ManageCoursePage.js
+++ ps-flux/src/components/ManageCoursePage.js
@@ -1,9 +1,10 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import CourseForm from "./CourseForm";
 import * as courseApi from "../api/courseApi";
 import { toast } from "react-toastify";
 
 const ManageCoursePage = props => {
+  const [errors, setErrors] = useState({});
   const [course, setCourse] = useState({
     id: null,
     slug: "",
@@ -12,6 +13,13 @@ const ManageCoursePage = props => {
     category: ""
   });
 
+  useEffect(() => {
+    const slug = props.match.params.slug;
+    if (slug) {
+      courseApi.getCourseBySlug(slug).then(_course => setCourse(_course));
+    }
+  }, [props.match.params.slug]);
+
   function handleChange({ target }) {
     setCourse({
       ...course,
@@ -19,8 +27,23 @@ const ManageCoursePage = props => {
     });
   }
 
+  function formIsValid() {
+    const _errors = {};
+
+    if (!course.title) _errors.title = "Title is required";
+    if (!course.authorId) _errors.authorId = "Author ID is required";
+    if (!course.category) _errors.category = "Category is required";
+
+    setErrors(_errors);
+
+    return Object.keys(_errors).length === 0;
+  }
+
   function handleSubmit(event) {
     event.preventDefault();
+
+    if (!formIsValid()) return;
+
     courseApi.saveCourse(course).then(() => {
       props.history.push("/courses");
       toast.success("Course saved.");
@@ -34,6 +57,7 @@ const ManageCoursePage = props => {
         course={course}
         onChange={handleChange}
         onSubmit={handleSubmit}
+        errors={errors}
       />
     </>
   );
